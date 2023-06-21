1ec555a5edfb3c58bd23cdfe538a4a16b3f7ee9c
LLIycTPuK <ashliye@yandex.ru>
2023-06-21 14:20:37 +0300

    Example005

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "artem")
+{
+    Console.WriteLine("Ура, это же АРТЕМ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+    }
